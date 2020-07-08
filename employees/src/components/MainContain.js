9a11b4f4c74e7f1c66a292497477768642b2d5d0
alex-waite <alexanderconorwaite@gmail.com>
2020-07-08 15:11:32 +0930

    tried Functional, swapping to class based here

--- employees/src/components/MainContain.js
+++ employees/src/components/MainContain.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+import GetEmployees from "../utils/API"
+
 import Jumbotron from "./Jumbotron"
 import Navbar from "./Navbar"
 
+
+
+
 function MainContain () {
+    state = { 
+        Employees: []
+    }
+
+    function renderEmployees() {
+        GetEmployees().then(results => {
+            this.setState({Employees: results.data.results})
+        })
+    }
+
+    componentDidMount(() => {
+        renderEmployees();
+    }) 
+
+
     return (
     <div className="MainContain">
         <Jumbotron />
