2b4523bec227370b80796577ce6ab25eb1277ea3
alex-waite <alexanderconorwaite@gmail.com>
2020-07-08 14:36:39 +0930

    React working well and structured nicely

---
+++ employees/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Jumbotron from "./components/Jumbotron"
+import Table from './components/Navbar';
+import MainContain from './components/MainContain';
+
+function App() {
+  return (
+    <div>
+      <MainContain />
+    </div>
+  );
+}
+
+export default App;
---
+++ employees/src/components/MainContain.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Jumbotron from "./Jumbotron"
+import Navbar from "./Navbar"
+
+function MainContain () {
+    return (
+    <div className="MainContain">
+        <Jumbotron />
+        <Navbar />
+    </div>
+    )}
+
+export default MainContain
